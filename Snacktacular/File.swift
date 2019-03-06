3ca7694150b2a2fea41c495badb48f5715c9ae1a
Alex Karacaoglu <alexkaracaoglu@alexs-mbp.home>
2019-03-05 22:13:56 -0500

    7.2 - Create Spot and Spots and update UI

---
+++ Snacktacular/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Snacktacular
+//
+//  Created by Alex Karacaoglu on 3/5/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Alex Karacaoglu on 3/5/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
