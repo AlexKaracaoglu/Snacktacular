e303fe6820ef0478373a168c984325061d3e598b
Alex Karacaoglu <alexkaracaoglu@alexs-mbp.home>
2019-03-06 11:21:25 -0500

    9.3 - Firebase app and Google sign in

--- Snacktacular/AppDelegate.swift
+++ Snacktacular/AppDelegate.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import GooglePlaces
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSPlacesClient.provideAPIKey("AIzaSyDl7AZFed9EPxFRlFCAmiBjdICVP13uXlE")
+        FirebaseApp.configure()
         return true
     }
 
